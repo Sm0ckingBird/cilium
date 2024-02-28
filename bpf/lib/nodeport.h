Sm0ckingBird/cilium

commit 0b2b039fcdba3c65f665a23d3476eb9b3ff116b4

dsr: add a new config option ipipv4cni to separate ipv4 and ipv6 dsr
Previously, we (bytedance) reuse the old config -- "bpf-lb-dsr-dispatch: ipip"
to enable ipip dsr for ipv4. This one will cause ipv6 to not working
because that setting is applied for both ipv4 and ipv6, but ipv6
ipip is not changed to support cni mode (it can only be used in
lb-only mode). Also, we probably will not change ipv6 dsr to use ipip. As a
result, this commmit add a new value setting for ipip v4 dsr. That is

bpf-lb-dsr-dispatch: ipipv4cni

The new setting allow us to enable ipip dsr only for ipv4, and ipv6 dsr
will keep use the old default dsr mode, which is using ip options.

Signed-off-by: Jiang Wang <jiang.wang@bytedance.com>

bpf/lib/nodeport.h
@@ -34,6 +34,7 @@
 #ifndef DSR_ENCAP_MODE
 #define DSR_ENCAP_MODE 0
 #define DSR_ENCAP_IPIP 2
+#define DSR_ENCAP_IPIPV4_CNI 3
 #endif
 #if defined(ENABLE_IPV4) && defined(ENABLE_MASQUERADE) && !defined(IPV4_MASQUERADE)
 #define IPV4_MASQUERADE 0
@@ -302,7 +303,7 @@ static __always_inline int dsr_set_ipip6(struct __ctx_buff *ctx,
 		return DROP_WRITE_ERROR;
 	return 0;
 }
-#elif DSR_ENCAP_MODE == DSR_ENCAP_NONE
+#elif DSR_ENCAP_MODE == DSR_ENCAP_NONE || DSR_ENCAP_MODE == DSR_ENCAP_IPIPV4_CNI
 static __always_inline int dsr_set_ext6(struct __ctx_buff *ctx,
 					struct ipv6hdr *ip6,
 					union v6addr *svc_addr,
@@ -526,7 +527,7 @@ int tail_nodeport_ipv6_dsr(struct __ctx_buff *ctx)
 #if DSR_ENCAP_MODE == DSR_ENCAP_IPIP
 	ret = dsr_set_ipip6(ctx, ip6, &addr,
 			    ctx_load_meta(ctx, CB_HINT), &ohead);
-#elif DSR_ENCAP_MODE == DSR_ENCAP_NONE
+#elif DSR_ENCAP_MODE == DSR_ENCAP_NONE || DSR_ENCAP_MODE == DSR_ENCAP_IPIPV4_CNI
 	ret = dsr_set_ext6(ctx, ip6, &addr,
 			   ctx_load_meta(ctx, CB_PORT), &ohead);
 #else
@@ -739,8 +740,11 @@ static __always_inline int nodeport_lb6(struct __ctx_buff *ctx,
 
 	svc = lb6_lookup_service(&key, false);
 	if (svc) {
-		const bool skip_l3_xlate = DSR_ENCAP_MODE == DSR_ENCAP_IPIP;
-
+#if DSR_ENCAP_MODE == DSR_ENCAP_IPIP
+		const bool skip_l3_xlate = true;
+#else
+		const bool skip_l3_xlate = false;
+#endif
 		if (!lb6_src_range_ok(svc, (union v6addr *)&ip6->saddr))
 			return DROP_NOT_IN_SRC_RANGE;
 
@@ -842,7 +846,7 @@ redo_local:
 			ctx_store_meta(ctx, CB_ADDR_V6_2, tuple.daddr.p2);
 			ctx_store_meta(ctx, CB_ADDR_V6_3, tuple.daddr.p3);
 			ctx_store_meta(ctx, CB_ADDR_V6_4, tuple.daddr.p4);
-#elif DSR_ENCAP_MODE == DSR_ENCAP_NONE
+#elif DSR_ENCAP_MODE == DSR_ENCAP_NONE || DSR_ENCAP_MODE == DSR_ENCAP_IPIPV4_CNI
 			ctx_store_meta(ctx, CB_PORT, key.dport);
 			ctx_store_meta(ctx, CB_ADDR_V6_1, key.address.p1);
 			ctx_store_meta(ctx, CB_ADDR_V6_2, key.address.p2);
@@ -1226,7 +1230,7 @@ static __always_inline int nodeport_nat_ipv4_fwd(struct __ctx_buff *ctx)
 }
 
 #ifdef ENABLE_DSR
-#if DSR_ENCAP_MODE == DSR_ENCAP_IPIP
+#if DSR_ENCAP_MODE == DSR_ENCAP_IPIP || DSR_ENCAP_MODE == DSR_ENCAP_IPIPV4_CNI
 static __always_inline __be32 rss_gen_src4(__be32 client, __be32 l4_hint)
 {
 	const __u32 bits = 32 - IPV4_RSS_PREFIX_BITS;
@@ -1244,7 +1248,7 @@ static __always_inline __be32 rss_gen_src4(__be32 client, __be32 l4_hint)
  *                  [clientIP:clientPort -> backendIP:backendPort]
  *                  [IP option: servicePort, serviceIP]} IP/L4
  */
-static __always_inline int dsr_set_ipip4(struct __ctx_buff *ctx,
+static __always_inline int dsr_set_ipip4cni(struct __ctx_buff *ctx,
 					 const struct iphdr *ip4,
 					 __be32 backend_addr,
 					 __be32 l4_hint,
@@ -1340,9 +1344,70 @@ static __always_inline int dsr_set_ipip4(struct __ctx_buff *ctx,
 		return DROP_CSUM_L3;
 	return 0;
 }
+
+/*
+ * Original packet: [clientIP:clientPort -> serviceIP:servicePort] } IP/L4
+ *
+ * After DSR IPIP:  [rssSrcIP -> backendIP]                        } IP
+ *                  [clientIP:clientPort -> serviceIP:servicePort] } IP/L4
+ */
+static __always_inline int dsr_set_ipip4(struct __ctx_buff *ctx,
+					 const struct iphdr *ip4,
+					 __be32 backend_addr,
+					 __be32 l4_hint, int *ohead)
+{
+	__u16 tot_len = bpf_ntohs(ip4->tot_len) + sizeof(*ip4);
+	const int l3_off = ETH_HLEN;
+	__be32 sum;
+	struct {
+		__be16 tot_len;
+		__be16 id;
+		__be16 frag_off;
+		__u8   ttl;
+		__u8   protocol;
+		__be32 saddr;
+		__be32 daddr;
+	} tp_old = {
+		.tot_len	= ip4->tot_len,
+		.ttl		= ip4->ttl,
+		.protocol	= ip4->protocol,
+		.saddr		= ip4->saddr,
+		.daddr		= ip4->daddr,
+	}, tp_new = {
+		.tot_len	= bpf_htons(tot_len),
+		.ttl		= IPDEFTTL,
+		.protocol	= IPPROTO_IPIP,
+		.saddr		= rss_gen_src4(ip4->saddr, l4_hint),
+		.daddr		= backend_addr,
+	};
+
+	if (dsr_is_too_big(ctx, tot_len)) {
+		*ohead = sizeof(*ip4);
+		return DROP_FRAG_NEEDED;
+	}
+
+	if (ctx_adjust_hroom(ctx, sizeof(*ip4), BPF_ADJ_ROOM_NET,
+			     ctx_adjust_hroom_dsr_flags()))
+		return DROP_INVALID;
+	sum = csum_diff(&tp_old, 16, &tp_new, 16, 0);
+	if (ctx_store_bytes(ctx, l3_off + offsetof(struct iphdr, tot_len),
+			    &tp_new.tot_len, 2, 0) < 0)
+		return DROP_WRITE_ERROR;
+	if (ctx_store_bytes(ctx, l3_off + offsetof(struct iphdr, ttl),
+			    &tp_new.ttl, 2, 0) < 0)
+		return DROP_WRITE_ERROR;
+	if (ctx_store_bytes(ctx, l3_off + offsetof(struct iphdr, saddr),
+			    &tp_new.saddr, 8, 0) < 0)
+		return DROP_WRITE_ERROR;
+	if (l3_csum_replace(ctx, l3_off + offsetof(struct iphdr, check),
+			    0, sum, 0) < 0)
+		return DROP_CSUM_L3;
+	return 0;
+}
 #endif /* DSR_ENCAP_MODE */
 
-#if DSR_ENCAP_MODE == DSR_ENCAP_NONE || DSR_ENCAP_MODE == DSR_ENCAP_IPIP
+#if DSR_ENCAP_MODE == DSR_ENCAP_NONE || DSR_ENCAP_MODE == DSR_ENCAP_IPIPV4_CNI
+
 static __always_inline int dsr_set_opt4(struct __ctx_buff *ctx,
 					struct iphdr *ip4, __be32 svc_addr,
 					__be32 svc_port, bool externalip,
@@ -1402,12 +1467,12 @@ static __always_inline int dsr_set_opt4(struct __ctx_buff *ctx,
 }
 #endif /* DSR_ENCAP_MODE */
 
-static __always_inline int handle_dsr_v4(struct __ctx_buff *ctx, bool *dsr)
+static __always_inline int handle_dsr_v4(struct __ctx_buff *ctx __maybe_unused, bool *dsr __maybe_unused)
 {
-	void *data, *data_end;
-	struct iphdr *ip4;
+	void *data __maybe_unused, *data_end __maybe_unused;
+	struct iphdr *ip4 __maybe_unused;
 
-#if DSR_ENCAP_MODE == DSR_ENCAP_IPIP
+#if DSR_ENCAP_MODE == DSR_ENCAP_IPIPV4_CNI
 	{
 		struct iphdr iph_inner __maybe_unused;
 		const int l3_off __maybe_unused = ETH_HLEN;
@@ -1661,7 +1726,7 @@ int tail_nodeport_ipv4_dsr(struct __ctx_buff *ctx)
 		goto drop_err;
 	}
 
-#if DSR_ENCAP_MODE == DSR_ENCAP_IPIP
+#if DSR_ENCAP_MODE == DSR_ENCAP_IPIPV4_CNI
 	ret = dsr_set_opt4(ctx, ip4,
 			   ctx_load_meta(ctx, CB_ADDR_V4_2),
 			   ctx_load_meta(ctx, CB_SRC_PORT),
@@ -1677,15 +1742,19 @@ int tail_nodeport_ipv4_dsr(struct __ctx_buff *ctx)
 		goto drop_err;
 	}
 
-	ret = dsr_set_ipip4(ctx, ip4,
+	ret = dsr_set_ipip4cni(ctx, ip4,
 				ctx_load_meta(ctx, CB_ADDR_V4),
 				ctx_load_meta(ctx, CB_HINT),
 				ctx_load_meta(ctx, CB_SRC_PORT),
 				ctx_load_meta(ctx, CB_ADDR_V4_2),
 				ctx_load_meta(ctx, CB_SVC_TYPE),
 				&ohead, false);
+#elif DSR_ENCAP_MODE == DSR_ENCAP_IPIP
+	ret = dsr_set_ipip4(ctx, ip4,
+			    ctx_load_meta(ctx, CB_ADDR_V4),
+			    ctx_load_meta(ctx, CB_HINT), &ohead);
 
-#elif DSR_ENCAP_MODE == DSR_ENCAP_NONE
+#elif DSR_ENCAP_MODE == DSR_ENCAP_NONE 
 	ret = dsr_set_opt4(ctx, ip4,
 			   ctx_load_meta(ctx, CB_ADDR_V4),
 			   ctx_load_meta(ctx, CB_PORT),
@@ -1871,7 +1940,7 @@ static __always_inline nodeport_ipv4_dsr(struct __ctx_buff *ctx)
 		goto drop_err;
 	}
 
-#if DSR_ENCAP_MODE == DSR_ENCAP_IPIP
+#if DSR_ENCAP_MODE == DSR_ENCAP_IPIPV4_CNI
 	ret = dsr_set_opt4(ctx, ip4,
 			   ctx_load_meta(ctx, CB_ADDR_V4_2),
 			   ctx_load_meta(ctx, CB_SRC_PORT),
@@ -1887,7 +1956,7 @@ static __always_inline nodeport_ipv4_dsr(struct __ctx_buff *ctx)
 		goto drop_err;
 	}
 
-	ret = dsr_set_ipip4(ctx, ip4,
+	ret = dsr_set_ipip4cni(ctx, ip4,
 				ctx_load_meta(ctx, CB_ADDR_V4),
 				ctx_load_meta(ctx, CB_HINT),
 				ctx_load_meta(ctx, CB_SRC_PORT),
@@ -1956,7 +2025,12 @@ static __always_inline int nodeport_lb4(struct __ctx_buff *ctx,
 
 	svc = lb4_lookup_service(&key, false);
 	if (svc) {
-		bool skip_l3_xlate = false;
+#if DSR_ENCAP_MODE == DSR_ENCAP_IPIP
+		const bool skip_l3_xlate = true;
+#else
+		const bool skip_l3_xlate = false;
+#endif
+
 
 		if (!lb4_src_range_ok(svc, ip4->saddr))
 			return DROP_NOT_IN_SRC_RANGE;
@@ -2075,7 +2149,7 @@ redo_local:
 	{
 		edt_set_aggregate(ctx, 0);
 		if (nodeport_uses_dsr4(&tuple)) {
-#if DSR_ENCAP_MODE == DSR_ENCAP_IPIP
+#if DSR_ENCAP_MODE == DSR_ENCAP_IPIPV4_CNI
 			ctx_store_meta(ctx, CB_HINT,
 				       ((__u32)tuple.sport << 16) | tuple.dport);
 			if (backend_local)
@@ -2085,12 +2159,17 @@ redo_local:
 			ctx_store_meta(ctx, CB_ADDR_V4_2, key.address);
 			ctx_store_meta(ctx, CB_SRC_PORT, key.dport);
 			ctx_store_meta(ctx, CB_SVC_TYPE, externalip_svc);
+#elif DSR_ENCAP_MODE == DSR_ENCAP_IPIP
+			ctx_store_meta(ctx, CB_HINT,
+				       ((__u32)tuple.sport << 16) | tuple.dport);
+			ctx_store_meta(ctx, CB_ADDR_V4, tuple.daddr);
+
 #elif DSR_ENCAP_MODE == DSR_ENCAP_NONE
 			ctx_store_meta(ctx, CB_PORT, key.dport);
 			ctx_store_meta(ctx, CB_ADDR_V4, key.address);
 #endif /* DSR_ENCAP_MODE */
 
-#if DSR_ENCAP_MODE == DSR_ENCAP_IPIP
+#if DSR_ENCAP_MODE == DSR_ENCAP_IPIPV4_CNI
 			if (!backend_local)
 				ep_tail_call(ctx, CILIUM_CALL_IPV4_NODEPORT_DSR);
 			else {

bpf/Makefile
@@ -200,7 +200,9 @@ XDP_OPTIONS = $(LB_OPTIONS) \
 	-DENABLE_NODEPORT_ACCELERATION:-DENABLE_IPV4:-DENABLE_IPV6:-DENABLE_HOST_SERVICES_UDP:-DENABLE_HOST_SERVICES_TCP:-DENABLE_NODEPORT:-DENABLE_DSR:-DENABLE_DSR_HYBRID: \
 	-DENABLE_NODEPORT_ACCELERATION:-DENABLE_IPV4:-DENABLE_IPV6:-DENABLE_NODEPORT:-DENABLE_DSR:-DDSR_ENCAP_MODE:-DDSR_ENCAP_NONE:-DDSR_ENCAP_IPIP=2 \
 	-DENABLE_NODEPORT_ACCELERATION:-DENABLE_IPV4:-DENABLE_IPV6:-DENABLE_NODEPORT:-DENABLE_DSR:-DDSR_ENCAP_MODE:-DDSR_ENCAP_IPIP:-DDSR_ENCAP_NONE=2 \
+	-DENABLE_NODEPORT_ACCELERATION:-DENABLE_IPV4:-DENABLE_IPV6:-DENABLE_NODEPORT:-DENABLE_DSR:-DDSR_ENCAP_MODE:-DDSR_ENCAP_IPIPV4_CNI:-DDSR_ENCAP_NONE=2 \
 	-DENABLE_NODEPORT_ACCELERATION:-DENABLE_IPV4:-DENABLE_IPV6:-DENABLE_NODEPORT:-DENABLE_DSR:-DENABLE_CAPTURE:-DDSR_ENCAP_MODE:-DDSR_ENCAP_NONE:-DDSR_ENCAP_IPIP=2 \
+	-DENABLE_NODEPORT_ACCELERATION:-DENABLE_IPV4:-DENABLE_IPV6:-DENABLE_NODEPORT:-DENABLE_DSR:-DENABLE_CAPTURE:-DDSR_ENCAP_MODE:-DDSR_ENCAP_IPIPV4_CNI:-DDSR_ENCAP_NONE=2 \
 	-DENABLE_NODEPORT_ACCELERATION:-DENABLE_IPV4:-DENABLE_IPV6:-DENABLE_NODEPORT:-DENABLE_DSR:-DENABLE_CAPTURE:-DDSR_ENCAP_MODE:-DDSR_ENCAP_IPIP:-DDSR_ENCAP_NONE=2
 
 ifndef MAX_XDP_OPTIONS
@@ -232,6 +234,7 @@ LXC_OPTIONS = \
 	 -DHAVE_LPM_TRIE_MAP_TYPE: \
 	 -DHAVE_LRU_HASH_MAP_TYPE: \
 	 -DENABLE_IPV4: \
+	 -DENABLE_NODEPORT_ACCELERATION:-DENABLE_IPV4:-DENABLE_IPV6:-DENABLE_NODEPORT:-DENABLE_DSR:-DDSR_ENCAP_MODE:-DDSR_ENCAP_IPIPV4_CNI:-DDSR_ENCAP_NONE=2 \
 	 -DENABLE_IPV6: \
 	 -DENABLE_IPV4:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DPOLICY_VERDICT_NOTIFY: \
 	 -DENABLE_IPV4:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DPOLICY_VERDICT_NOTIFY:-DENABLE_IPSEC:-DIP_POOLS: \

bpf/bpf_lxc.c
@@ -1462,7 +1462,7 @@ skip_policy_enforcement:
 		if (ret == CT_REOPENED)
 			ct_update4_dsr(get_ct_map4(&tuple), &tuple, dsr);
 
-#if DSR_ENCAP_MODE == DSR_ENCAP_IPIP
+#if DSR_ENCAP_MODE == DSR_ENCAP_IPIPV4_CNI
 		if (!revalidate_data(ctx, &data, &data_end, &ip4))
 			return DROP_INVALID;
 		tuple.nexthdr = ip4->protocol;

daemon/cmd/daemon_main.go
@@ -611,7 +611,7 @@ func initializeFlags() {
 	flags.String(option.LoadBalancerAlg, option.NodePortAlgRandom, "BPF load balancing algorithm (\"random\", \"maglev\")")
 	option.BindEnv(option.LoadBalancerAlg)
 
-	flags.String(option.LoadBalancerDSRDispatch, option.DSRDispatchOption, "BPF load balancing DSR dispatch method (\"opt\", \"ipip\")")
+	flags.String(option.LoadBalancerDSRDispatch, option.DSRDispatchOption, "BPF load balancing DSR dispatch method (\"opt\", \"ipip\", \"ipipv4cni\")")
 	option.BindEnv(option.LoadBalancerDSRDispatch)
 
 	flags.String(option.LoadBalancerDSRL4Xlate, option.DSRL4XlateFrontend, "BPF load balancing DSR L4 DNAT method for IPIP (\"frontend\", \"backend\")")

daemon/cmd/kube_proxy_replacement.go
@@ -105,7 +105,8 @@ func initKubeProxyReplacementOptions() (bool, error) {
 
 		if option.Config.NodePortMode == option.NodePortModeDSR &&
 			option.Config.LoadBalancerDSRDispatch != option.DSRDispatchOption &&
-			option.Config.LoadBalancerDSRDispatch != option.DSRDispatchIPIP ||
+			option.Config.LoadBalancerDSRDispatch != option.DSRDispatchIPIP &&
+			option.Config.LoadBalancerDSRDispatch != option.DSRDispatchIPIPV4CNI ||
 			option.Config.NodePortMode == option.NodePortModeHybrid &&
 				option.Config.LoadBalancerDSRDispatch != option.DSRDispatchOption {
 			return false, fmt.Errorf("Invalid value for --%s: %s", option.LoadBalancerDSRDispatch, option.Config.LoadBalancerDSRDispatch)
@@ -350,6 +351,13 @@ func initKubeProxyReplacementOptions() (bool, error) {
 
 		if option.Config.NodePortMode == option.NodePortModeDSR &&
 			option.Config.LoadBalancerDSRDispatch == option.DSRDispatchIPIP {
+			if option.Config.NodePortAcceleration == option.NodePortAccelerationDisabled {
+				return false, fmt.Errorf("DSR dispatch mode %s currently only available under XDP acceleration", option.Config.LoadBalancerDSRDispatch)
+			}
+		}
+
+		if option.Config.NodePortMode == option.NodePortModeDSR &&
+			option.Config.LoadBalancerDSRDispatch == option.DSRDispatchIPIPV4CNI {
 			if option.Config.NodePortAcceleration == option.NodePortAccelerationDisabled {
 				return false, fmt.Errorf("DSR dispatch mode %s currently only available under XDP acceleration", option.Config.LoadBalancerDSRDispatch)
 			}

pkg/datapath/linux/config/config.go
@@ -306,6 +306,7 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 			dsrEncapInv = iota
 			dsrEncapNone
 			dsrEncapIPIP
+			dsrEncapIPIPCNI
 		)
 		const (
 			dsrL4XlateInv = iota
@@ -313,6 +314,7 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 			dsrL4XlateBackend
 		)
 		cDefinesMap["DSR_ENCAP_IPIP"] = fmt.Sprintf("%d", dsrEncapIPIP)
+		cDefinesMap["DSR_ENCAP_IPIPV4_CNI"] = fmt.Sprintf("%d", dsrEncapIPIPCNI)
 		cDefinesMap["DSR_ENCAP_NONE"] = fmt.Sprintf("%d", dsrEncapNone)
 		cDefinesMap["DSR_XLATE_FRONTEND"] = fmt.Sprintf("%d", dsrL4XlateFrontend)
 		cDefinesMap["DSR_XLATE_BACKEND"] = fmt.Sprintf("%d", dsrL4XlateBackend)
@@ -329,8 +331,12 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 				cDefinesMap["DSR_ENCAP_MODE"] = fmt.Sprintf("%d", dsrEncapNone)
 			} else if option.Config.LoadBalancerDSRDispatch == option.DSRDispatchIPIP {
 				cDefinesMap["DSR_ENCAP_MODE"] = fmt.Sprintf("%d", dsrEncapIPIP)
+			} else if option.Config.LoadBalancerDSRDispatch == option.DSRDispatchIPIPV4CNI {
+				cDefinesMap["DSR_ENCAP_MODE"] = fmt.Sprintf("%d", dsrEncapIPIPCNI)
 			}
-			if option.Config.LoadBalancerDSRDispatch == option.DSRDispatchIPIP {
+
+			if option.Config.LoadBalancerDSRDispatch == option.DSRDispatchIPIP ||
+				option.Config.LoadBalancerDSRDispatch == option.DSRDispatchIPIPV4CNI {
 				if option.Config.LoadBalancerDSRL4Xlate == option.DSRL4XlateFrontend {
 					cDefinesMap["DSR_XLATE_MODE"] = fmt.Sprintf("%d", dsrL4XlateFrontend)
 				} else if option.Config.LoadBalancerDSRL4Xlate == option.DSRL4XlateBackend {

pkg/option/config.go
@@ -1106,6 +1106,9 @@ const (
 	// DSR dispatch mode to encapsulate to IPIP
 	DSRDispatchIPIP = "ipip"
 
+	// DSR dispatch mode to encapsulate to IPIP
+	DSRDispatchIPIPV4CNI = "ipipv4cni"
+
 	// DSR L4 translation to frontend port
 	DSRL4XlateFrontend = "frontend"
 
@@ -1744,7 +1747,7 @@ type DaemonConfig struct {
 	NodePortAlg string
 
 	// LoadBalancerDSRDispatch indicates the method for pushing packets to
-	// backends under DSR ("opt" or "ipip")
+	// backends under DSR ("opt", "ipip", or "ipipv4cni")
 	LoadBalancerDSRDispatch string
 
 	// LoadBalancerDSRL4Xlate indicates the method for L4 DNAT translation

commit b9dc538005081b5fee426227b2657b08b7551933

xdp: Fix exteranl ip 1hop case does not use vip regression
In commit 0b2b039fcd, we changed config name from ipip to ipipv4cni.
But it missed one case when the 1 hop has the backend. As a result, the
connection is good, but backend pod does no see the vip. Fix it in
this patch.

Fixes: 0b2b039fcd ("dsr: add a new config option ipipv4cni")
Signed-off-by: Jiang Wang <jiang.wang@bytedance.com>

bpf/lib/nodeport.h
@@ -2143,7 +2143,7 @@ redo_local:
 		}
 	}
 
-#if DSR_ENCAP_MODE != DSR_ENCAP_IPIP
+#if DSR_ENCAP_MODE != DSR_ENCAP_IPIPV4_CNI
 	if (!backend_local)
 #endif
 	{