0e304ded71cca654cef92b69c5bf25bab7140c2b
Zijian Zhang <zijianzhang@bytedance.com>
2024-12-14 01:30:36 +0000

    pkg/endpoint: make state synchronization atomic
    commit acf3141f359679d9a827cc517377ccffa7b99cc2 upstream.
    
    BPF regeneration writes state into a new temporary directory. Once it
    has succeeded we need to swap the old and new directory. This is currently
    achieved by "backing up" the current state by renaming the directory.
    This code has a bunch of corner cases around cleaning up old directories
    and so on which are necessary since the synchronization isn't truly
    atomic.
    
    Instead, use the RENAME_EXCHANGE flag to atomically exchange the two
    existing directories. Also use hard links to retain existing state
    so that killing the agent during a synchronization doesn't lead
    to corruption.
    
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>
    Signed-off-by: Zijian Zhang <zijianzhang@bytedance.com>

--- pkg/endpoint/directory_test.go
+++ pkg/endpoint/directory_test.go
@@ -74,7 +74,7 @@ func (s *EndpointSuite) TestMoveNewFilesTo(c *check.C) {
 		},
 	}
 	for _, tt := range tests {
-		if err := moveNewFilesTo(tt.args.oldDir, tt.args.newDir); (err != nil) != tt.wantErr {
+		if err := copyExistingState(tt.args.oldDir, tt.args.newDir); (err != nil) != tt.wantErr {
 			c.Assert(err != nil, check.Equals, tt.wantErr)
 			compareDir(tt.args.oldDir, tt.wantOldDir)
 			compareDir(tt.args.newDir, tt.wantNewDir)
@@ -102,7 +102,7 @@ func benchmarkMoveNewFilesTo(b *testing.B, numFiles int) {
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		if err := moveNewFilesTo(oldDir, newDir); err != nil {
+		if err := copyExistingState(oldDir, newDir); err != nil {
 			b.Fatal(err)
 		}
 	}
